e908fbe8db48ae5490d00addfc387de7d2d24aa6
Grace <grace@gracebook.local>
2020-01-21 15:34:05 -0800

    Edit: heroku backend axios links in src actions folder auth js file.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -28,7 +28,7 @@ export const loadUser = () => (dispatch, getState) => {
         config.headers['Authorization'] = `Token ${token}`;
     }
     axios
-      .get('https://grace-bank-frontend.herokuapp.com/users/api/auth/user', config)
+      .get('https://grace-bank-backend.herokuapp.com/users/api/auth/user', config)
       .then(res => {
           dispatch({
             type: USER_LOADED,
@@ -53,7 +53,7 @@ export const login = (username, password) => dispatch => {
     // Request Body 
     const body = JSON.stringify({ username, password });
     axios
-      .post('https://grace-bank-frontend.herokuapp.com/users/api/auth/login', body, config)
+      .post('https://grace-bank-backend.herokuapp.com/users/api/auth/login', body, config)
       .then(res => {
           dispatch({
             type: LOGIN_SUCCESS,
@@ -79,7 +79,7 @@ export const register = ({ username, email, password }) => dispatch => {
     // Request Body 
     const body = JSON.stringify({ username, email, password });
     axios
-      .post('https://grace-bank-frontend.herokuapp.com/users/api/auth/register', body, config)
+      .post('https://grace-bank-backend.herokuapp.com/users/api/auth/register', body, config)
       .then(res => {
           dispatch({
             type: REGISTER_SUCCESS,
@@ -111,7 +111,7 @@ export const logout = () => (dispatch, getState) => {
         config.headers['Authorization'] = `Token ${token}`;
     }
     axios
-      .post('https://grace-bank-frontend.herokuapp.com/users/api/auth/logout', null, config)
+      .post('https://grace-bank-backend.herokuapp.com/users/api/auth/logout', null, config)
       .then(res => {
           dispatch({
             type: LOGOUT_SUCCESS,
